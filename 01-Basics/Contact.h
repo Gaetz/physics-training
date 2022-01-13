9c9c201316a2cc68a16ee08758e26283ebb62d1f
Gaetz <gaetz@rufflerim.com>
2022-01-13 15:37:58 +0100

    Physics basics start

---
+++ 01-Basics/Contact.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "code/Math/Vector.h"
+#include "Body.h"
+class Contact
+{
+public:
+	Vec3 ptOnAWorldSpace;
+	Vec3 ptOnALocalSpace;
+	Vec3 ptOnBWorldSpace;
+	Vec3 ptOnBLocalSpace;
+
+	Vec3 normal;
+	float separationDistance;
+	float timeOnImpact;
+
+	Body* a{ nullptr };
+	Body* b{ nullptr };
+
+	static void ResolveContact(Contact& contact);
+};
