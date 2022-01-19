75b923de2ea164e59f7dce4c8b99959b3826e253
Gaetz <gaetz@rufflerim.com>
2022-01-19 19:44:56 +0100

    Start second lesson

--- 01-Basics/Shape.cpp
+++ 01-Basics/Shape.cpp
@@ -26,3 +26,29 @@ Bounds ShapeSphere::GetBounds() const
 	tmp.maxs = Vec3(radius);
 	return tmp;
 }
+
+Vec3 ShapeSphere::Support(const Vec3& dir, const Vec3& pos, const Quat& orient, const float bias)
+{
+	return pos + dir * (radius + bias);
+}
+
+void ShapeBox::Build(const Vec3* pts, const int num)
+{
+	for (int i = 0; i < num; ++i)
+	{
+		bounds.Expand(points[i]);
+	}
+
+	points.clear();
+	points.push_back(Vec3{ bounds.mins.x, bounds.mins.y, bounds.mins.z });
+	points.push_back(Vec3{ bounds.maxs .x, bounds.mins.y, bounds.mins.z });
+	points.push_back(Vec3{ bounds.mins.x, bounds.maxs.y, bounds.mins.z });
+	points.push_back(Vec3{ bounds.mins.x, bounds.mins.y, bounds.maxs.z });
+
+	points.push_back(Vec3{ bounds.maxs.x, bounds.maxs.y, bounds.maxs.z });
+	points.push_back(Vec3{ bounds.mins.x, bounds.maxs.y, bounds.maxs.z });
+	points.push_back(Vec3{ bounds.maxs.x, bounds.mins.y, bounds.maxs.z });
+	points.push_back(Vec3{ bounds.maxs.x, bounds.maxs.y, bounds.mins.z });
+
+	centerOfMass = (bounds.maxs + bounds.mins) * 0.5f;
+}
--- 01-Basics/Shape.h
+++ 01-Basics/Shape.h
@@ -3,12 +3,14 @@
 #include "code/Math/Bounds.h"
 #include "code/Math/Quat.h"
 
-class Shape 
+class Shape
 {
 public:
 	enum class ShapeType
 	{
-		SHAPE_SPHERE
+		SHAPE_SPHERE,
+		SHAPE_BOX,
+		SHAPE_CONVEX
 	};
 
 	virtual ShapeType GetType() const = 0;
@@ -17,11 +19,15 @@ public:
 	virtual Bounds GetBounds(const Vec3& pos, const Quat& orient) const = 0;
 	virtual Bounds GetBounds() const = 0;
 
+	virtual void Build(const Vec3* pts, const int num) {}
+	virtual Vec3 Support(const Vec3& dir, const Vec3& pos, const Quat& orient, const float bias) = 0;
+	virtual float FastestLinearSpeed(const Vec3& angularVelocity, const Vec3& dir) const { return 0; }
+
 protected:
 	Vec3 centerOfMass;
 };
 
-class ShapeSphere : public Shape 
+class ShapeSphere : public Shape
 {
 public:
 	ShapeSphere(float radiusP) : radius(radiusP)
@@ -33,7 +39,29 @@ public:
 	Mat3 InertiaTensor() const override;
 	Bounds GetBounds(const Vec3& pos, const Quat& orient) const override;
 	Bounds GetBounds() const override;
+	Vec3 Support(const Vec3& dir, const Vec3& pos, const Quat& orient, const float bias) override;
 
 	float radius;
+};
+
+class ShapeBox : public Shape
+{
+public:
+	ShapeBox(const Vec3* points, const int num)
+	{
+		Build(points, num);
+
+	}
+
+	ShapeType GetType() const override { return ShapeType::SHAPE_BOX; }
+	Mat3 InertiaTensor() const override;
+	Bounds GetBounds(const Vec3& pos, const Quat& orient) const override;
+	Bounds GetBounds() const override;
+	void Build(const Vec3* pts, const int num) override;
+	Vec3 Support(const Vec3& dir, const Vec3& pos, const Quat& orient, const float bias) override;
+	float FastestLinearSpeed(const Vec3& angularVelocity, const Vec3& dir) const override;
+
+	std::vector<Vec3> points;
+	Bounds bounds;
 };
 
