22578c2d74148fd8d04d83654d07b266ba12825c
Gaëtan Blaise-Cazalet <gaetan.blaise-cazalet@artfx.fr>
2025-01-14 11:10:35 +0100

    [feature] Vector 3 class

--- PhysicsEngine/01.Particles/gphysics/Vec3.cpp
+++ PhysicsEngine/01.Particles/gphysics/Vec3.cpp
@@ -11,21 +11,78 @@ namespace gphysics {
         z = -z;
     }
 
-    real Vec3::Magnitude() {
+    real Vec3::Magnitude() const {
         return RealSqrt(x * x + y * y + z * z);
     }
 
-    real Vec3::SquareMagnitude() {
+    real Vec3::SquareMagnitude() const {
         return x * x + y * y + z * z;
     }
 
     void Vec3::Normalize() {
         real length = Magnitude();
         if (length > 0) {
-            const real invLength = static_cast<real>(1) / length;
-            x *= invLength;
-            y *= invLength;
-            z *= invLength;
+            (*this) *= ((real)1)/length;
         }
+    }
+
+    void Vec3::operator*=(const real scalar) {
+        x *= scalar;
+        y *= scalar;
+        z *= scalar;
+    }
+
+    Vec3 Vec3::operator*(const real scalar) const {
+        return Vec3(x * scalar, y * scalar, z * scalar);
+    }
+
+    void Vec3::operator+=(const Vec3& v) {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+    }
+
+    Vec3 Vec3::operator+(const Vec3& v) const {
+        return Vec3(x + v.x, y + v.y, z + v.z);
+    }
+
+    void Vec3::operator-=(const Vec3& v) {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+    }
+
+    Vec3 Vec3::operator-(const Vec3& v) const {
+        return Vec3(x - v.x, y - v.y, z - v.z);
+    }
+
+    void Vec3::AddScaledVector(const Vec3& v, const real scale) {
+        x += v.x * scale;
+        y += v.y * scale;
+        z += v.z * scale;
+    }
+
+    void Vec3::MakeOrthonormalBasis(Vec3& a, Vec3& b, Vec3& c) {
+        a.Normalize();
+        c = a % b;
+        if (c.SquareMagnitude() == 0) {
+            return;
+        }
+        c.Normalize();
+        b = c % a;
+    }
+
+    Vec3 Vec3::ComponentProduct(const Vec3& v) const {
+        return Vec3(x * v.x, y * v.y, z * v.z);
+    }
+
+    void Vec3::ComponentProductUpdate(const Vec3& v) {
+        x *= v.x;
+        y *= v.y;
+        z *= v.z;
+    }
+
+    void Vec3::operator%=(const Vec3& v) {
+        *this = Cross(v);
     }
 }
--- PhysicsEngine/01.Particles/gphysics/Vec3.hpp
+++ PhysicsEngine/01.Particles/gphysics/Vec3.hpp
@@ -21,9 +21,51 @@ namespace gphysics {
         Vec3(real x, real y, real z) : x(x), y(y), z(z), pad(0) {}
 
         void Invert();
-        real Magnitude();
-        real SquareMagnitude();
+        real Magnitude() const;
+        real SquareMagnitude() const;
         void Normalize();
+
+        void operator*=(real scalar);
+        Vec3 operator*(real scalar) const;
+        void operator+=(const Vec3& v);
+        Vec3 operator+(const Vec3& v) const;
+        void operator-=(const Vec3& v);
+        Vec3 operator-(const Vec3& v) const;
+        void AddScaledVector(const Vec3& v, real scale);
+
+        /** Makes an orthonormal basis from vector, that won't change direction.
+         * Vector b and c will be oriented so that the base is orthonormal.
+         * All vectors will be normalized.
+         * This function is working for a right-handed coordinate system.
+         */
+        static void MakeOrthonormalBasis(Vec3& a, Vec3& b, Vec3& c);
+
+        /** Calculate and return a component-wise product of this vector with the given vector. */
+        Vec3 ComponentProduct(const Vec3& v) const;
+
+        /** Perform a component-wise product with the given vector and set this vector to its result. */
+        void ComponentProductUpdate(const Vec3& v);
+
+        /** Calculate and return the scalar product of this vector with the given vector. */
+        real ScalarProduct(const Vec3& v) const { return x * v.x + y * v.y + z * v.z; }
+
+        /** Calculate and return the scalar product of this vector with the given vector. */
+        real Dot(const Vec3& v) { return x * v.x + y * v.y + z * v.z; }
+
+        /** Calculate and return the scalar product of this vector with the given vector. */
+        real operator*(const Vec3& v) const { return x * v.x + y * v.y + z * v.z;}
+
+        /** Calculate and return the vector product of this vector with the given vector. */
+        Vec3 VectorProduct(const Vec3& v) const { return Vec3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+
+        /** Calculate and return the vector product of this vector with the given vector. */
+        Vec3 Cross(const Vec3& v) const { return Vec3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+
+        /** Calculate and return the vector product of this vector with the given vector. */
+        Vec3 operator%(const Vec3& v) const { return Vec3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+
+        /** Updates this vector to be the vector product of its current value and the given vector. */
+        void operator%=(const Vec3& v);
     };
 }
 
