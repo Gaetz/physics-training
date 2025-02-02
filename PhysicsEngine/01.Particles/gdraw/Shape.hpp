e887e8402b581e8dba62e818fc9aac107b8c4aaf
Gaëtan Blaise-Cazalet <gaetan.blaisecazalet@gmail.com>
2025-02-02 16:39:20 +0100

    [prototype] Quaternion and shape

---
+++ PhysicsEngine/01.Particles/gdraw/Shape.hpp
@@ -0,0 +1,14 @@
+//
+// Created by gaetz on 02/02/2025.
+//
+
+#ifndef GDRAW_SHAPE_HPP
+#define GDRAW_SHAPE_HPP
+
+
+class Shape {
+
+};
+
+
+#endif //GDRAW_SHAPE_HPP
---
+++ PhysicsEngine/01.Particles/gmath/Quaternion.cpp
@@ -0,0 +1,9 @@
+//
+// Created by gaetz on 02/02/2025.
+//
+
+#include "Quaternion.hpp"
+
+namespace gmath {
+    const Quaternion Quaternion::Identity(0.0f, 0.0f, 0.0f, 1.0f);
+}
